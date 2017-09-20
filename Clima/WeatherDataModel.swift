1ba4ff1418d34b89f1019960628bde1a8395f2e7
Stanley Liao <aegis2469111@gmail.com>
2017-09-20 19:16:05 +0800

    Initial Commit

---
+++ Clima/WeatherDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDataModels.swift
+//  Clima
+//
+//  Created by Stanley on 2017/9/20.
+//  Copyright © 2017年 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDataModel {
+    
+}
